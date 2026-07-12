420212311d7eac15b6aba48087a57c61bd0ddd98
SofiDev <angiemeow89@gmail.com>
2026-07-11 20:36:02 -0600

    🚧 Se crea hook para actualizar PUT

--- src/hooks/useUpdateEmployee.ts
+++ src/hooks/useUpdateEmployee.ts
@@ -1,12 +1,45 @@
 import { useState } from "react";
 import { apiFetch } from "@/lib/api";
-
 import type { Employee } from "@/types/employee.type";
+import { useRouter } from "next/router";
+
 
 const useUpdateEmployee = () => {
-    return (
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState<Error | null>(null);
+    const router = useRouter();
+    const UpdateEmployee = async (id: string, updatedFields: Partial<Employee>) => {
+        setLoading(true);
+        setError(null);
+        try {
+            const payload = {
+                name: updatedFields.name,
+                lastname: updatedFields.lastname,
+                email: updatedFields.email,
+                nss: updatedFields.nss,
+                rfc: updatedFields.rfc,
+                position: updatedFields.position,
+                salary: updatedFields.salary,
+                isActive: updatedFields.isActive,
+                createdAt: updatedFields.createdAt,
+            }
+            await apiFetch(`/employees/${id}`, {
+                method: "PUT",
+                body: payload,
+            })
+            router.reload();
+            router.push(`/dashboard/employees/${id}`);
+        } catch (error) {
+            setError(error as Error);
+        } finally {
+            setLoading(false);
+        }
+    }
+    return {
+        loading,
+        error,
+        UpdateEmployee,
+    };
+};
 
-    )
-}
-
-export default useUpdateEmployee
+export default useUpdateEmployee;
